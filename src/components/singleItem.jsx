4d62c92c642132a6b978eab587b7f4916195c9bf
unnati sangraula <unnatisangraula@gmail.com>
2026-02-17 22:13:02 +0545

    singleItemjsx

--- src/components/singleItem.jsx
+++ src/components/singleItem.jsx
@@ -2,8 +2,28 @@ import { FiEdit, FiTrash2 } from "react-icons/fi";
 
 const SingleItem = ({item}) => {
    return(
-    <div>
-        singleitem
+    <div className="single-item">
+      <input type="checkbox" checked={item.completed} onChange={() => null} />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: item.completed ? "line-through" : "none",
+        }}
+      >
+        {item.name}
+      </p>
+
+      <button className="btn icon-btn" type="button" onClick={() => null}>
+        <FiEdit size={18} />
+      </button>
+
+      <button
+        className="btn icon-btn remove-btn"
+        type="button"
+        onClick={() => null}
+      >
+        <FiTrash2 size={18} />
+      </button>
     </div>
    )
 }

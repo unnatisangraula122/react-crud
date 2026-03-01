9044b64f20ca6ce1120b20e22abbaba10fce992a
unnati sangraula <unnatisangraula@gmail.com>
2026-03-01 20:13:29 +0545

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import Items from "./components/Items";
 import { groceryItems } from "./data/groceryItems";
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [items, setItems] = useState(groceryItems);
+
+  const editCompleted = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+
   return (
     <section className="section-center">
-      <Items items={groceryItems} />
+      <Items items={items} editCompleted={editCompleted} />
     </section>
   );
 };
